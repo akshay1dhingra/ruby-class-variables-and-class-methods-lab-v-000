ad71e61bac35ec8b5bd6919c66cf01d40c3c02e3
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-11-28 02:51:47 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+require 'pry'
+
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @@count+=1
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.artist_count
+    artists = {}
+
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    binding.pry
+  #  genres = {}
+      #g = @@genres.each {|genre| }
+  end
+
+
+end
